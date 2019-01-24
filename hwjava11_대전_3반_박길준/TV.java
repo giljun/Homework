3b082b64940583d7ae68bea5365aa56e0a3f26b4
박길준 <adda281@naver.com>
2019-01-24 11:23:16 +0000

    2019.01.24 java homework TV.java

---
+++ hwjava11_대전_3반_박길준/TV.java
@@ -0,0 +1,44 @@
+package com.ssafy;
+
+import java.io.Serializable;
+
+public class TV extends Product implements Serializable{
+	private int inch;
+	private String type;
+	
+	// 기본 생성자.
+	public TV() {
+		super();
+	}
+	
+	public TV(int num, String name, int price, int stock, int inch, String type) {
+		super(num, name, price, stock);
+		this.inch = inch;
+		this.type = type;
+	}
+
+	public TV(int num, String name, int price, int stock) {
+		this( num, name, price, stock, 0, "");
+	}
+
+	public int getInch() {
+		return inch;
+	}
+
+	public void setInch(int inch) {
+		this.inch = inch;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "TV [ " + super.toString() + ", inch = " + inch + ", type = " + type + " ]";
+	}
+}
