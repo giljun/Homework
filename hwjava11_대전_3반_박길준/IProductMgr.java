2fc0a07ff137de1f8b821a6c128a023b64054540
박길준 <adda281@naver.com>
2019-01-24 11:19:32 +0000

    2019.01.24 java homework IProductMgr.java

---
+++ hwjava11_대전_3반_박길준/IProductMgr.java
@@ -0,0 +1,39 @@
+package com.ssafy;
+
+import java.util.ArrayList;
+
+public interface IProductMgr {
+	void add(Product product) throws DuplicateException;
+
+	ArrayList<Product> searchAll();
+
+	Product searchByNum(int num) throws CodeNotFoundException;
+
+	ArrayList<Product> searchByName(String name);
+
+	ArrayList<TV> searchOnlyTV();
+
+	ArrayList<Refrigerator> searchOnlyRefigerators();
+
+	ArrayList<Refrigerator> searchByAboveRefrirator(int capacity) throws ProductNotFoundException;
+
+	ArrayList<TV> searchByAboveTV(int inch) throws ProductNotFoundException;
+
+	void updateProduct(int num, int price);
+
+	void deleteProduct(int num);
+
+	void TotalPriceOfStock();
+	
+	// 파일을 오픈한다.
+	void open();
+	
+	// 파일을 저장한다.
+	void close();
+	
+	void openObj();
+	
+	void closeObj();
+	
+	
+}
