67c4c713f420ab150974dff3208a03094a5d7b1f
박길준 <adda281@naver.com>
2019-01-31 22:04:00 +0000

    3. Customer.java

---
+++ hwjava14_대전_3반_박길준/Customer.java
@@ -0,0 +1,59 @@
+package com.ssafy.edu;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable{
+	private String name;
+	private String phone;
+	private String address;
+	
+	public Customer(String name, String phone, String address) {
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+	}
+	
+	public Customer(String name, String phone) {
+		this(name, phone, "");
+	}
+
+	public Customer() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(name);
+		sb.append("   ");
+		sb.append(phone);
+		sb.append("   ");
+		sb.append(address);
+		return sb.toString();
+	}
+	
+	
+}
